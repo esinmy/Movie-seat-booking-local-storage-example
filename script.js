b4c93f060bc81d808df059e4d40488cfb8297389
Maxim Esin <esinmy@gmail.com>
2020-08-01 22:44:03 +0300

    Added saving selection to local storage

--- script.js
+++ script.js
@@ -7,23 +7,34 @@ const movieSelect = document.querySelector("#movie");
 let ticketPrice = +movieSelect.value;
 
 function updateSelectedCount() {
-  const selectedSeats = document.querySelectorAll(".row .selected");
-  const selectedSeatsCount = selectedSeats.length;
-  count.innerText = selectedSeatsCount;
-  total.innerText = selectedSeatsCount * ticketPrice;
+    const selectedSeats = document.querySelectorAll(".row .selected");
+
+    const seatIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatIndex));
+
+    const selectedSeatsCount = selectedSeats.length;
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
 }
 
 container.addEventListener("click", (e) => {
-  if (
-    e.target.classList.contains("seat") &&
-    !e.target.classList.contains("occupied")
-  ) {
-    e.target.classList.toggle("selected");
-    updateSelectedCount();
-  }
+    if (
+        e.target.classList.contains("seat") &&
+        !e.target.classList.contains("occupied")
+    ) {
+        e.target.classList.toggle("selected");
+        updateSelectedCount();
+    }
 });
 
 movieSelect.addEventListener("change", (e) => {
-  ticketPrice = +e.target.value;
-  updateSelectedCount();
+    ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
+    updateSelectedCount();
 });
